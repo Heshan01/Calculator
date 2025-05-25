ad97cb74ebebb220096793892fa53038196d25ec
Heshan Dilhara <113496185+Heshan01@users.noreply.github.com>
2025-05-24 22:54:33 -0700

    Add files via upload
    💻 Technologies Used
    Java (Swing for GUI)
    
    Apache NetBeans 26 IDE
    
    ✨ Features
    User-friendly interface
    
    Real-time calculation display
    
    Handles basic arithmetic: +, -, *, /
    
    Error handling for invalid operations (e.g., divide by zero)

---
+++ Calculator_v1/src/main/java/com/mycompany/calculator_v1/Calculator_v1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator_v1;
+
+/**
+ *
+ * @author Heshan
+ */
+public class Calculator_v1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
